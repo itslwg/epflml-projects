7bea974f6a155a28180e705aa352c4fc5406dda6
nathansimonis1612 <nathansimonis@hotmail.com>
2020-10-22 17:25:35 +0200

    Add early stopping

--- Project_1/utils/test.py
+++ Project_1/utils/test.py
@@ -107,7 +107,7 @@ print(f"Regularized implementation accuracy: {accuracy(y, y_pred)}")
 lambda_ = 0.05
 reg = 2 # -> L2 or L1 regularization.
 w, loss = reg_logistic_regression(y, tx, lambda_, reg, initial_w,
-                                  max_it, gamma, batch_size=1)
+                                  1000, gamma, batch_size=1, verbose=True)
 y_pred = np.rint(sigmoid(tx @ w))
 print(f"Regularized implementation accuracy: {accuracy(y, y_pred)}")
 
